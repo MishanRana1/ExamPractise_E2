7660dfa4888bce5e3387628e6f7ec5f90576c55e
MishanRana1 <mishanrana12@gmail.com>
2025-09-29 18:53:45 +0000

    Bird class done.

---
+++ Bird.java
@@ -0,0 +1,25 @@
+public class Bird implements Flight{
+
+    private String type;
+
+    public Bird(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I’m a bird who flaps my wings to fly.");
+    }
+
+    @Override
+    public String toString() {
+        return "Bird [type=" + type + "]: ";
+    }
+}

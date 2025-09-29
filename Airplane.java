340f5c3e8819897fcda309148e86eb0eb58a8ce0
MishanRana1 <mishanrana12@gmail.com>
2025-09-29 18:42:51 +0000

    Airplane class done.

---
+++ Airplane.java
@@ -0,0 +1,34 @@
+public class Airplane implements Flight{
+
+    private String model;
+    private int yearBuilt;
+
+    public Airplane(String model, int yearBuilt){
+        this.model = model;
+        this.yearBuilt = yearBuilt;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public int getYearBuilt() {
+        return yearBuilt;
+    }
+    public void setYearBuilt(int yearBuilt){
+        this.yearBuilt = yearBuilt;
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I’m an airplane that relies on an engine to fly.");
+    }
+
+    @Override
+    public String toString() {
+        return "Airplane " + "[model=" + model + ", year=" + year + "]";
+    }
+}
